353a069a7712f1800d3e958f047176e6b2604b7f
saltyfish17 <135342399+saltyfish17@users.noreply.github.com>
2024-02-20 16:15:34 +0800

    Update DesktopLauncher.java
    Spacedefender

--- TEAM4-GDXEngine/desktop/src/com/mygdx/game/DesktopLauncher.java
+++ TEAM4-GDXEngine/desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -10,6 +10,6 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("GDXEngine");
-		new Lwjgl3Application(new MyGdxGame(), config);
+		new Lwjgl3Application(new Spacedefender(), config);
 	}
 }
